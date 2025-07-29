df21175ed35e91c59327ef060052e0c63d164225
notmuzammil <muzammil16oct2003@gmail.com>
2025-07-30 03:42:36 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Contact from "./pages/Contact";
 import Vendors from "./pages/Vendors";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
+import AIChatWidget from "./components/AIChatWidget";
 
 const queryClient = new QueryClient();
 
@@ -29,6 +30,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <AIChatWidget />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
