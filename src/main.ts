b3576eab1a93800fa802041a21bb741ad13f062c
bawanyabdulrehman <bawanyabdulrehman@gmail.com>
2025-08-10 05:18:21 +0500

    new code

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Remove trailing slash from CORS_ORIGIN
+  const corsOrigin = process.env.CORS_ORIGIN?.replace(/\/$/, '');
+
+  app.enableCors({
+    origin: [corsOrigin, 'https://aasaan-shaadi.vercel.app'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
